aeba6c40537b47784776500c4dc55643ffef302a
altgokul@gmail.com <altgokul@users.noreply.github.com>
2016-06-24 10:31:41 +0530

    removed unused code and irrelevant comments

--- extension.js
+++ extension.js
@@ -59,14 +59,12 @@ let Indicator = new Lang.Class({
 	},
 
     _sync: function() {
-        // Do we have batteries or a UPS?
         let visible = this._proxy.Online;
 		log('ACPowerIndicator: sync, visible: '  + visible);
         if (visible) {
         	this.icon.icon_name = this._proxy.IconName;
 			this.icon.style_class =  'system-status-icon';
         } else {
-            // If there's no battery, then we use the power icon.
             this.icon.icon_name = 'battery-empty-symbolic';
 			this.icon.style_class =  'system-status-icon-alert';
         }
@@ -77,28 +75,14 @@ let Indicator = new Lang.Class({
 let button, indicator;
 
 function init() {
-	/*
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: false,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: false });
-    let icon = new St.Icon({ icon_name: 'ac-adapter-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-	*/
 	indicator = new Indicator();
 }
 
 function enable() {
     Main.panel._rightBox.insert_child_at_index(indicator.button, 0);
-	log('ACPowerIndicator: enabled ');
 }
 
 function disable() {
     Main.panel._rightBox.remove_child(indicator.button);
-	log('ACPowerIndicator: disabled');
 }
 
